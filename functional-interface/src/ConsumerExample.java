8219b9ca2343c6cf23a9c1cbab2dca8f6c2ab364
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-06-05 12:43:10 -0300

    exemplos de cosumer e supplier

---
+++ functional-interface/src/ConsumerExample.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) throws Exception {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer t) {
+                if (t % 2 == 0) {
+                    System.out.println(t);
+                }
+            }
+        });
+    }
+}
---
+++ functional-interface/src/SupplierExample.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Olá bem vindo(a)";
+
+        List<String> listaSaudacoes = Stream.generate(saudacao).limit(5).toList();
+
+        listaSaudacoes.forEach(n -> System.out.println(n));
+    }
+}
